1bdb0aceb4e9a6b2c7a66a18e7aa149f4cddc890
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2018-12-07 17:50:12 +0900

    Website updates are now propagated to S3 and Cloudfront (#18)
    * Added the structure for a simple backend
    
    * deleted a lock file
    
    * restructured the monkey-web a bit
    
    * made the build.sh script generic
    
    * Website updates are now propagated to S3 and Cloudfront
    
    * fixed an invalidation bug

--- services/monkey/web/main.tf
+++ services/monkey/web/main.tf
@@ -7,10 +7,24 @@ data "terraform_remote_state" "common_infra" {
 }
 
 resource "null_resource" "upload_web_app" {
+  triggers {
+    asset_manifest = "${md5(file("${path.module}/build/asset-manifest.json"))}"
+    manifest       = "${md5(file("${path.module}/build/manifest.json"))}"
+    index          = "${md5(file("${path.module}/build/index.html"))}"
+    favicon        = "${md5(file("${path.module}/build/favicon.ico"))}"
+  }
+
   provisioner "local-exec" {
     command = <<EOF
     aws s3 sync --profile sandbox ${path.module}/build/ s3://${data.terraform_remote_state.common_infra.website_bucket_id} &&
-    aws cloudfront create-invalidation --profile sandbox --distribution-id ${data.terraform_remote_state.common_infra.website_cloudfront_id} --paths /*
+    aws cloudfront create-invalidation --profile sandbox --distribution-id ${data.terraform_remote_state.common_infra.website_cloudfront_id} --paths "/*"
+    EOF
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+    command = <<EOF
+    aws s3 rm --profile sandbox s3://${data.terraform_remote_state.common_infra.website_bucket_id} --recursive
     EOF
   }
 }

7ce9b17debc3596e2661fd06d18914c2f23c32f4
Fredrik Lönnblad <f.lonnblad@gmail.com>
2018-12-24 11:36:34 +0100

    Fixed a bad reference to modules from common/base

--- common/base/main.tf
+++ common/base/main.tf
@@ -16,21 +16,21 @@ provider "aws" {
 }
 
 module "public_zone" {
-  source = "../../modules/public_zone"
+  source = "../modules/public_zone"
 
   domain_name         = "${var.root_domain_name}"
   root_hosted_zone_id = "${var.root_hosted_zone_id}"
 }
 
 module "website" {
-  source = "../../modules/website"
+  source = "../modules/website"
 
   aws_route53_zone_id = "${module.public_zone.zone_id}"
   root_domain_name    = "${var.root_domain_name}"
 }
 
 module "remote_state" {
-  source = "../../modules/remote_state"
+  source = "../modules/remote_state"
 
   table_name  = "${var.remote_state_table_name}"
   bucket_name = "${var.remote_state_bucket_name}"
